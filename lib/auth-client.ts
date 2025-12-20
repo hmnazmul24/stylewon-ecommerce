a933ecde08e0b00fcb32617b760f6ed899648d22
shoubj-ahmed <codegreen19s@gmail.com>
2025-12-20 15:50:51 +0600

    auth client base url changed

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,7 @@
+import "dotenv";
 import { createAuthClient } from "better-auth/react";
 import { emailOTPClient, phoneNumberClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [phoneNumberClient(), emailOTPClient()],
 });
